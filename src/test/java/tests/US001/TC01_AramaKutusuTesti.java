d60c2bf059da9c1729747b15a9f1b056fdee11b5
Lenovo <gokayterzi@gmail.com>
2023-07-25 11:51:48 +0300

    US001_V2

--- src/test/java/tests/US001/TC01_AramaKutusuTesti.java
+++ src/test/java/tests/US001/TC01_AramaKutusuTesti.java
@@ -14,5 +14,6 @@ public class TC01_AramaKutusuTesti {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
         Page page = new Page();
         page.aramaKutusu.sendKeys("iphone", Keys.ENTER);
+        Driver.closeDriver();
     }
 }
